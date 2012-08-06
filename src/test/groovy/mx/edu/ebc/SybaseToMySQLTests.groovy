f284684116ca85cbe25048e3cc62f026039d23cd
neodevelop <neodevelop@gmail.com>
2012-08-06 13:59:34 -0500

    Usando nombres de tablas particulares para pruebas

--- src/test/groovy/mx/edu/ebc/SybaseToMySQLTests.groovy
+++ src/test/groovy/mx/edu/ebc/SybaseToMySQLTests.groovy
@@ -19,7 +19,17 @@ class SybaseToMySQLTests extends groovy.util.GroovyTestCase {
   }
 
   void testCountRowsForAllTables(){
-    def tableNames = dbInfo.tableNames
+    //def tableNames = dbInfo.tableNames
+    def tableNames = ["acceso_gafete",
+      "acceso_items",
+      "acceso_motivo",
+      //"acceso_persons",
+      "acceso_rel_items_persons",
+      "acceso_status",
+      //"actividad",
+      //"actividad_espacio",
+      "actividad_subactividad",
+      "actividades"]
     def rowsPerTable = new DB().countRowsPerTable(tableNames)
     rowsPerTable.each { k,v ->
       log.info "$k tiene $v registros"
