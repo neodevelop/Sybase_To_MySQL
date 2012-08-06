921397d8ef69aa6e184bcbe612985e28cf1d7266
neodevelop <neodevelop@gmail.com>
2012-08-06 16:13:09 -0500

    Agregando una prueba para migracion asyncrona

---
+++ src/test/groovy/mx/edu/ebc/SybaseToMySQLOneTableAsyncTests.groovy
@@ -0,0 +1,34 @@
+package org.edu.ebc
+
+import static groovyx.gpars.GParsPool.withPool
+import groovy.sql.*
+import groovy.util.logging.*
+import java.sql.*
+
+class SybaseToMySQLOneTableAsyncTests extends groovy.util.GroovyTestCase{
+  
+  def tables
+
+  void setUp(){
+    def table1 = new TablaAMigrar()
+    table1.tableName = "actividades"
+    def table2 = new TablaAMigrar()
+    table2.tableName = "acceso_gafete"
+    def table3 = new TablaAMigrar()
+    table3.tableName = "bajas_acad_periodos"
+    tables = [table1,table2,table3]
+  }
+
+  void testMigrate(){
+    log.info "Iniciando migración"
+    withPool{
+      log.info "With pool start"
+      tables.eachParallel{ t ->
+        def u = t.migrate()
+        log.info "$u"
+      }
+      log.info "With pool end"
+    }
+    log.info "Terminando migración"
+  }
+}
