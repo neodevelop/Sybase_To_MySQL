699ced24e133d568f22949accf03a382ccc51922
neodevelop <neodevelop@gmail.com>
2012-08-07 13:26:44 -0500

    Agregando la prueba de conteo async

--- src/test/groovy/mx/edu/ebc/SybaseToMysqlAsyncTests.groovy
+++ src/test/groovy/mx/edu/ebc/SybaseToMysqlAsyncTests.groovy
@@ -1,4 +1,25 @@
 package mx.edu.ebc
 
-class SybaseToMysqlAsyncTests {
+import static groovyx.gpars.GParsPool.withPool
+
+class SybaseToMysqlAsyncTests extends GroovyTestCase {
+  void setUp(){
+
+  }
+  void testCountAsync(){
+    def tableNames
+    DB.instance.withSybaseInstance { sql ->
+      tableNames = new DBInfo().getTableNamesNoCurry(sql)
+    }
+    log.info "${tableNames}"
+    withPool {
+      DB.instance.withSybaseInstance { sql ->
+        tableNames.eachParallel { tn ->
+          def dbInfo = new DBInfo()
+          def c = dbInfo.countRowsPerTable(tn)
+          log.info "$tn tiene $c registros"
+        }
+      }
+    }
+  }
 }
